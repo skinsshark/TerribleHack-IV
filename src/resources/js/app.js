4b7d99a8ea419c6dc22f7d1ecdf10ae4d1808abe
szheng <sharon.zheng@live.com>
2016-07-23 15:10:53 -0400

    youtube api

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,10 +1,80 @@
-$(function() {
 
-});
 
-function init() {
-	gapi.client.setApiKey("AIzaSyD4KXnN-f5YuPnymtetxD2_JK9anHGSq3g");
-	gapi.client.load("youtube" , "v3" , function() {
-		// youtube api ready
-	});
+// Your use of the YouTube API must comply with the Terms of Service:
+// https://developers.google.com/youtube/terms
+
+// Helper function to display JavaScript value on HTML page.
+function showResponse(response) {
+    var responseString = JSON.stringify(response, '', 2);
+    // document.getElementById('response').innerHTML += responseString;
+    console.log(responseString);
 }
+
+// Called automatically when JavaScript client library is loaded.
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+
+// Called automatically when YouTube API interface is loaded (see line 9).
+function onYouTubeApiLoad() {
+    // This API key is intended for use only in this lesson.
+    // See https://goo.gl/PdPA1 to get a key for your own applications.
+    gapi.client.setApiKey('AIzaSyD4KXnN-f5YuPnymtetxD2_JK9anHGSq3g');
+
+    search();
+}
+
+function search() {
+    // Use the JavaScript client library to create a search.list() API call.
+    var request = gapi.client.youtube.search.list({
+        part: 'snippet',
+        
+    });
+    
+    // Send the request to the API server,
+    // and invoke onSearchRepsonse() with the response.
+    request.execute(onSearchResponse);
+}
+
+// Called automatically with the response of the YouTube API request.
+function onSearchResponse(response) {
+    showResponse(response);
+}
+
+
+
+
+
+
+
+// $(function() {
+
+// });
+
+// function init() {
+// 	gapi.client.setApiKey("AIzaSyD4KXnN-f5YuPnymtetxD2_JK9anHGSq3g");
+// 	gapi.client.load("youtube" , "v3" , function() {
+// 		// youtube api ready
+// 	});
+// }
+
+
+// // After the API loads, call a function to enable the search box.
+// function handleAPILoaded() {
+//   $('#search-button').attr('disabled', false);
+// }
+
+// // Search for a specified string.
+// function search() {
+//   var q = $('#query').val();
+//   var request = gapi.client.youtube.search.list({
+//     q: q,
+//     part: 'snippet'
+//   });
+
+//   request.execute(function(response) {
+//     var str = JSON.stringify(response.result);
+//     $('#search-container').html('<pre>' + str + '</pre>');
+//   });
+// }
+
